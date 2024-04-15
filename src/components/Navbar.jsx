aecbbbde9040f9715c8a47b87a49bdd4a3b35918
hannaholiero <hannahostebo@gmail.com>
2024-04-15 20:01:32 +0200

    Added base for Homepage and Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from "react";
-import styles from "./Navbar.module.css"; // Säkerställ att detta är rätt sökväg till din CSS-modul
+import styles from "./Navbar.module.css";
+import "../index.css";
 
-const Navbar = () => {
-  return <nav className={styles.navbar}>HEJ</nav>;
-};
+function Navbar() {
+  return (
+    <nav className="header">
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <h1>Twitterish</h1>
+        </div>
+        <div className={styles.menu}>
+          <ul>
+            <li>
+              <a href="/">Hem</a>
+            </li>
+            <li>
+              <a href="/">Profil</a>
+            </li>
+            <li>
+              <a href="/">Upptäck</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
