6830e55363995418dd99772cf074d7f5dd177b55
hannaholiero <hannahostebo@gmail.com>
2024-04-15 15:24:15 +0200

    Created Homepage, Navbar, Login mm

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+import CssBaseline from "@mui/material/CssBaseline";
+function Homepage() {
+  return (
+    <div>
+      <CssBaseline />
+      <Header />
+      <Hero />
+      <Section />
+      <AboutUs />
+      <Testimonial />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
+}
+
+export default Homepage;
