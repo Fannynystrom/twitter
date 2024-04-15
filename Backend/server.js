b62324d1698991a2bdc6d19fa6496947d7710017
Fannosa <fannynystrom@outlook.comls>
2024-04-15 23:55:34 +0200

    connection to Mongodb Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 dotenv.config();
 
-
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; 
+
+// =============================MongoDB======================================================
+    const mongoUri = process.env.MONGODB_URI;
+      const twitterPostSchema = new mongoose.Schema({
+              content: String,
+            });
+    const TwitterPost = mongoose.model('TwitterPost', twitterPostSchema);
+
+    mongoose.connect(mongoUri)
+        .then(() => console.log('Ansluten till MongoDB'))
+        .catch(err => console.error('Kunde inte ansluta till MongoDB', err));
 
 
-app.get('/', (req, res) => {
-    res.send('Hej Twitterrrrrr!');
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Servern körs på http://localhost:${PORT}`);
-  });
+
+//==============================Routes===================================================== 
+ app.get('/', (req, res) => {
+ res.send('Hej Twitterrrrrr nu e vi på gång!');
+ });
+
+
+
+app.listen(PORT, () => {
+console.log(`Servern körs på http://localhost:${PORT}`);
+    });
