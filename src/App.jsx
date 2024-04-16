58766818ccfea674472270c9809523c16fec3d6e
Benjamin.Cahoon <benjamin.cahoon@hotmail.com>
2024-04-16 11:17:55 +0200

    testar för att se om nya branchen funkar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage.jsx";
+import LoginForm from "./components/Register.jsx";
+import RegisterForm from "./components/Register.jsx";
+
 import "./index.css";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+      
       </Routes>
     </Router>
   );
