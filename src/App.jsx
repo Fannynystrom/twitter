144a2943345968d7549f2c336ab9c391774a4764
Benjamin.Cahoon <benjamin.cahoon@hotmail.com>
2024-04-16 19:48:06 +0200

    la till routes for login och reg.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react";
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import LoginForm from "./components/Register.jsx";
 import RegisterForm from "./components/Register.jsx";
@@ -10,7 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-      
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
       </Routes>
     </Router>
   );
