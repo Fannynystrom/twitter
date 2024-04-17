933ba92e31d42354354459c6b18d4df9a0bdc313
CarolineMagnenat <145575566+CarolineMagnenat@users.noreply.github.com>
2024-04-17 13:15:07 +0200

    carros console.log test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import LoginForm from "./components/Login.jsx";
 import RegisterForm from "./components/Register.jsx";
