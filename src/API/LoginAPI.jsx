850a5dc6fa07e0978686bbd38d68d5914bd6ad97
Benjamin.Cahoon <benjamin.cahoon@hotmail.com>
2024-04-18 10:39:37 +0200

    Lyckats koppla ihop med API.

--- src/API/LoginAPI.jsx
+++ src/API/LoginAPI.jsx
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:3000/login';
+const API_URL = "http://localhost:3000/api/login";
 
 export const loginUser = async (userData) => {
-    try {
-        const response = await axios.post(`${API_URL}users/login`, userData);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const response = await axios.post(API_URL, userData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
-console.log('Carro är bäst <3333')
-console.log('Carro är sämst<33333')
+console.log("Carro är bäst <3333");
+console.log("Carro är sämst<33333");
