d64353d8c0726c4e6c3d973c06d0ee6b842d2cc9
hannaholiero <hannahostebo@gmail.com>
2024-04-23 12:08:38 +0200

    Login fungerar - tweets sparas under rätt namn

--- src/API/RegisterAPI.jsx
+++ src/API/RegisterAPI.jsx
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:3000/api/users/register';
+const API_URL = "http://localhost:3000/api/users/register";
 
 export const registerUser = async (userData) => {
-    try {
-        const response = await axios.post(API_URL, userData);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const response = await axios.post(API_URL, userData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
