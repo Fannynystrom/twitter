82fdaefb0ed54e91c191fd516c8f3f4360c563a0
CarolineMagnenat <145575566+CarolineMagnenat@users.noreply.github.com>
2024-04-29 14:45:50 +0200

    # works with åäö&?!= and started on testing

---
+++ Backend/test/testdummy.test.js
@@ -0,0 +1,19 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import supertest from "supertest";
+
+import app from "../../src/App";
+const api = supertest(app);
+
+let mongoServer; 
+
+beforeEach(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+    await mongoose.connect(mongoUri);
+  });
+  
+  afterEach(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
