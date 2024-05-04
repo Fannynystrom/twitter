a2fe2cfe4e08565ae157deeede8060f8fbcd712a
CarolineMagnenat <145575566+CarolineMagnenat@users.noreply.github.com>
2024-05-04 18:35:25 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 // =============================MongoDB======================================================
 const mongoUri = process.env.MONGODB_URI;
-//const mongoUri = "mongodb+srv://carroadmin:GhjcrJ6oN5nOj5Ge@cluster0.ob6sik0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
